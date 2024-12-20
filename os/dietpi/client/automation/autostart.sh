32fdc20430335be3b01a83b98c9cb5f109718c59
Thomas Eleff <eleffthomas@gmail.com>
2024-12-20 12:51:29 -0600

    Adds client os automation autostart

---
+++ os/dietpi/client/automation/autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# DietPi-AutoStart custom script
+# Location: /var/lib/dietpi/dietpi-autostart/custom.sh
+audera run client
+exit 0
--- os/dietpi/client/automation/setup.sh
+++ os/dietpi/client/automation/setup.sh
@@ -12,29 +12,35 @@ WORKSPACE="/home/dietpi/audera"
 SHAIRPORT_CONFIG="/etc/shairport-sync.conf"
 REPO_SHAIRPORT_CONFIG="$WORKSPACE/os/dietpi/client/conf/shairport-sync.conf"
 
+AUTOSTART_SCRIPT="/var/lib/dietpi/dietpi-autostart/custom.sh"
+REPO_AUTOSTART_SCRIPT="$WORKSPACE/os/dietpi/client/automation/autostart.sh"
+
+# Start console logging
+echo " ________  ___  ___  ________  _______  ________  ________      "
+echo "|\   __  \|\  \|\  \|\   ___ \|\   ___\|\   __  \|\   __  \     "
+echo "\ \  \|\  \ \  \\\  \ \  \_|\ \ \  \__|\ \  \|\  \ \  \|\  \    "
+echo " \ \   __  \ \  \\\  \ \  \ \\ \ \   __\\ \      /\ \   __  \   "
+echo "  \ \  \ \  \ \  \\\  \ \  \_\\ \ \  \_|_\ \  \  \ \ \  \ \  \  "
+echo "   \ \__\ \__\ \______/\ \______/\ \______\ \__\\ _\\ \__\ \__\ "
+echo "    \|__|\|__|\|______| \|______| \|______|\|__|\|__|\|__|\|__| "
+echo
+echo ">>> Running the Audera playback-client setup & installation..."
+echo
+echo "    Script source {https://raw.githubusercontent.com/thomaseleff/audera/refs/heads/main/os/dietpi/client/automation/setup.sh}."
+
 # Ensure the script is running as root
+echo
 if [[ $EUID -ne 0 ]]; then
    echo "*** CRITICAL: The setup-script must be run as {sudo}." 
    exit 1
 fi
 
-# Install software
 
-#   id   software
-#   ---  --------------
-#   5    alsa
-#   7    ffmpeg
-#   17   git
-#   37   shairport-sync
-#   130  python3
-
-echo ">>> Installing optimized software"
-dietpi-software install 5 7 17 37 130
-
-echo ">>> Installing build package software"
+echo ">>> Installing build packages"
 apt-get update && apt-get install python3-dev build-essential python3-pyaudio portaudio19-dev -y
 
 # Clone the git repository
+echo
 if [ ! -d "$WORKSPACE" ]; then
   echo ">>> Cloning the Git repository"
   git clone -b main "$GIT_REPO_URL" "$WORKSPACE"
@@ -44,6 +50,7 @@ else
 fi
 
 # Replace shairport-sync configuration with the file from the repository
+echo
 if [ -f "$SHAIRPORT_CONFIG" ]; then
   echo ">>> Updating the shairport-sync configuration"
   cp "$REPO_SHAIRPORT_CONFIG" "$SHAIRPORT_CONFIG"
@@ -57,12 +64,30 @@ echo ">>> Restarting shairport-sync"
 systemctl restart shairport-sync
 
 # Install Python requirements
+echo
 if [ -f "$WORKSPACE/requirements.txt" ]; then
   echo ">>> Installing Python requirements"
-  pip3 install "$WORKSPACE"
+  pip3 install "$WORKSPACE" --root-user-action
 else
-  echo " ** ERROR: Python requirements not found."
+  echo " ** ERROR: Failed to build & install audera."
   exit 1
 fi
 
-echo ">>> Audera-playback-client setup completed successfully."
+# Set up the autostart script
+echo
+if [ ! -f "$SHAIRPORT_CONFIG" ]; then
+  echo ">>> Creating the custom autostart script"
+  cp "$REPO_AUTOSTART_SCRIPT" "$AUTOSTART_SCRIPT"
+  chmod +x "$AUTOSTART_SCRIPT"
+else
+  echo "  * WARNING: Autostart script not found."
+fi
+
+# Log
+echo
+echo "[ ok ] The Audera playback-client setup & installation completed successfully"
+
+# Restart
+echo ">>> Restarting the Audera playback-client"
+sleep 3
+reboot
