579c0499135c994e7aee0902d5f0e4c78b93ecc7
Thomas Eleff <eleffthomas@gmail.com>
2025-04-20 07:38:17 -0500

    Updates access-point

--- audera/ap.py
+++ audera/ap.py
@@ -1,16 +1,22 @@
 """ Access point management """
 
+from typing_extensions import Literal
 import subprocess
-from audera import struct, platform
+import time
+from audera import struct, platform, netifaces
 
 
 class AccessPoint():
-    """ A `class` that represents a wi-fi access point.
+    """ A `class` that represents a Wi-Fi access point.
 
     Parameters
     ----------
     name: `str`
         The name of the access-point.
+    url: `str`
+        The url web-address for accessing the access-point.
+    interface: `str`
+        The network interface for the access-point.
     identity: `audera.struct.identity.Identity`
         The `audera.struct.identity.Identity` containing the unique identity of the
             network device.
@@ -19,27 +25,35 @@ class AccessPoint():
     def __init__(
         self,
         name: str,
+        url: str,
+        interface: Literal['wlan0'],
         identity: struct.identity.Identity
     ):
-        """ Creates an instance of a wi-fi access point.
+        """ Creates an instance of a Wi-Fi access point.
 
         Parameters
         ----------
         name: `str`
             The name of the access-point.
+        url: `str`
+            The url web-address for accessing the access-point.
+        interface: `str`
+            The network interface for the access-point.
         identity: `audera.struct.identity.Identity`
             The `audera.struct.identity.Identity` containing the unique identity of the
                 network device.
         """
+        self.url = url.replace('https://', '').replace('http://')
+        self.interface = interface
         self.hostname = '-'.join([name.strip().lower(), identity.short_uuid])
 
     @platform.requires('dietpi')
     def start(self):
-        """ Starts a wi-fi access point for credential sharing. """
+        """ Starts a Wi-Fi access point for credential sharing. """
 
         # Configure hostapd
         with open("/etc/hostapd/hostapd.conf", "w") as f:
-            f.write("interface=wlan0\n")
+            f.write(f"interface={self.interface}\n")
             f.write("driver=nl80211\n")
             f.write(f"ssid={self.hostname}\n")
             f.write("hw_mode=g\n")
@@ -48,38 +62,97 @@ class AccessPoint():
             f.write("macaddr_acl=0\n")
             f.write("auth_algs=1\n")
             f.write("ignore_broadcast_ssid=0\n")
-            f.write("wpa=2\n")
-            f.write("rsn_pairwise=CCMP\n")
             f.write(f"hostname={self.hostname}\n")
 
-        # Enable hostapd
-        with open("/etc/default/hostapd", "a") as f:
+        # Update the hostapd service
+        with open("/etc/default/hostapd", "w") as f:
             f.write("DAEMON_CONF=\"/etc/hostapd/hostapd.conf\"\n")
 
         # Start hostapd
         subprocess.run(["sudo", "systemctl", "unmask", "hostapd"], check=True)
-        subprocess.run(["sudo", "systemctl", "enable", "hostapd"], check=True)
-        subprocess.run(["sudo", "systemctl", "start", "hostapd"], check=True)
+        hostapd_result = subprocess.run(["sudo", "systemctl", "start", "hostapd"], check=True)
+
+        # Get the default interface ip-address
+        interface_address = netifaces.get_interface_ip_address(self.interface)
+
+        # Wait for the service
+        if hostapd_result.returncode == 0:
+
+            # Check the service, time-out if the service fails to start after 10 seconds
+            time_out = 0
+
+            while time_out < 10:
+                time.sleep(1)
+
+                if self.hostapd_is_active():
+                    break
+
+                time_out += 1
+
+            if not self.hostapd_is_active():
+                raise AccessPointError(
+                    'Unable to start the Wi-Fi access-point {%s} on interface {%s}.' % (
+                        self.hostname,
+                        self.interface
+                    )
+                )
 
         # Configure dnsmasq
-        with open("/etc/dnsmasq.conf", "a") as f:
-            f.write("interface=wlan0\n")
-            f.write("dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h\n")
-
-        # Enable dnsmasq
-        subprocess.run(["sudo", "systemctl", "enable", "dnsmasq"], check=True)
+        with open("/etc/dnsmasq.conf", "w") as f:
+            f.write(f"interface={self.interface}\n")
+            f.write(f"address=/{self.url}/{interface_address}")
 
         # Start dnsmasq
-        subprocess.run(["sudo", "systemctl", "restart", "dnsmasq"], check=True)
+        subprocess.run(["sudo", "systemctl", "unmask", "dnsmasq"], check=True)
+        dnsmasq_result = subprocess.run(["sudo", "systemctl", "start", "dnsmasq"], check=True)
+
+        # Wait for the service
+        if dnsmasq_result.returncode == 0:
+
+            # Check the service, time-out if the service fails to start after 10 seconds
+            time_out = 0
+
+            while time_out < 10:
+                time.sleep(1)
+
+                if self.dnsmasq_is_active():
+                    break
+
+                time_out += 1
+
+            if not self.dnsmasq_is_active():
+                raise AccessPointError(
+                    'Unable to start the DNS server to route traffic from {%s} to {%s} on interface {%s}.' % (
+                        'https://%s' % self.url,
+                        interface_address,
+                        self.interface
+                    )
+                )
 
     @platform.requires('dietpi')
-    def stop(self):
-        """ Stops a wi-fi access point. """
+    def stop():
+        """ Stops a Wi-Fi access point. """
 
-        subprocess.run(
-            ['nmcli', 'connection', 'delete', self.hostname],
+        subprocess.run(['sudo', 'systemctl', 'stop', 'hostapd'], check=True)
+        subprocess.run(['sudo', 'systemctl', 'stop', 'dnsmasq'], check=True)
+
+    @platform.requires('dietpi')
+    def hostapd_is_active() -> bool:
+        """ Returns the active status of the Wi-Fi access point. """
+
+        return True if subprocess.run(
+            ['sudo', 'systemctl', 'is-active', 'hostapd'],
             check=True
-        )
+        ).stdout.decode().strip() == 'active' else False
+
+    @platform.requires('dietpi')
+    def dnsmasq_is_active() -> bool:
+        """ Returns the active status of the local DNS server service. """
+
+        return True if subprocess.run(
+            ['sudo', 'systemctl', 'is-active', 'dnsmasq'],
+            check=True
+        ).stdout.decode().strip() == 'active' else False
 
 
 # Exception(s)
